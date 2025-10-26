c0ad17ccbad9cac32769648111eb7c7790970636
AndrewBestCoder <greenlaptop@mail.ru>
2025-10-26 18:43:01 +0500

    Init

---
+++ MagicFactoryProj/Source/MagicFactoryProj/MagicFactoryProjGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MagicFactoryProjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICFACTORYPROJ_API AMagicFactoryProjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
